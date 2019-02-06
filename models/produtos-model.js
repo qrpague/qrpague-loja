ea9ad8b1bd3bcfcc0a06e33ceb5cbba2eee11266
leonardoSilveira <leo.compiler@gmail.com>
2019-02-06 19:28:17 -0200

    acetando path imagens

--- models/produtos-model.js
+++ models/produtos-model.js
@@ -4,10 +4,10 @@ let Config = require( global.pathRootApp + '/tools/config')
 
 let produtos = [
 
-      { id: 1, titulo: 'Smartphone LG K10 Novo Dual Chip Android 7.0 Tela 5,3" 32GB 4G 13MP - Preto', valor: 699.00, url:  ' ./images/p1.png', thumb:  ' ./images/thumb.png' },
-      { id: 2, titulo: 'Smartphone LG K10 Novo Dual Chip Android 7.0 Tela 5,3" 32GB 4G 13MP - Preto', valor: 699.00, url:   ' ./images/p2.png', thumb:  ' ./images/thumb.png' },
-      { id: 3, titulo: 'Smartphone LG K10 Novo Dual Chip Android 7.0 Tela 5,3" 32GB 4G 13MP - Preto', valor: 699.00, url:   ' ./images/p3.png', thumb:  ' ./images/thumb.png'},
-      { id: 4, titulo: 'Smartphone LG K10 Novo Dual Chip Android 7.0 Tela 5,3" 32GB 4G 13MP - Preto', valor: 699.00, url:   ' ./images/p4.png', thumb:  ' ./images/thumb.png'},
+      { id: 1, titulo: 'Smartphone LG K10 Novo Dual Chip Android 7.0 Tela 5,3" 32GB 4G 13MP - Preto', valor: 699.00, url: Config.GATEWAY_IMAGE +'images/p1.png', thumb: Config.GATEWAY_IMAGE +'images/thumb.png' },
+      { id: 2, titulo: 'Smartphone LG K10 Novo Dual Chip Android 7.0 Tela 5,3" 32GB 4G 13MP - Preto', valor: 699.00, url:  Config.GATEWAY_IMAGE +'images/p2.png', thumb: Config.GATEWAY_IMAGE +'images/thumb.png' },
+      { id: 3, titulo: 'Smartphone LG K10 Novo Dual Chip Android 7.0 Tela 5,3" 32GB 4G 13MP - Preto', valor: 699.00, url:  Config.GATEWAY_IMAGE +'images/p3.png', thumb: Config.GATEWAY_IMAGE +'images/thumb.png'},
+      { id: 4, titulo: 'Smartphone LG K10 Novo Dual Chip Android 7.0 Tela 5,3" 32GB 4G 13MP - Preto', valor: 699.00, url:  Config.GATEWAY_IMAGE +'images/p4.png', thumb: Config.GATEWAY_IMAGE +'images/thumb.png'},
  
 ]
 
