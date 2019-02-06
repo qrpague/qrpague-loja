789b4b5944544fb453f613a233e0efeb5b47a0d9
leonardoSilveira <leo.compiler@gmail.com>
2019-02-06 18:47:09 -0200

    acetando caminhos relativos a aplicacao.

--- public/js/main.js
+++ public/js/main.js
@@ -6,36 +6,36 @@ var app = angular.module('myApp', ['ngRoute']);
 app.config(['$routeProvider', '$locationProvider', function AppConfig($routeProvider, $locationProvider) {
     $routeProvider
         .when("/", {
-            templateUrl: "../views/home.html",
+            templateUrl: "./views/home.html",
             controller: "HomeController"
 
         })
         .when("/produto-detalhe", {
-            templateUrl: "../views/produto-detalhe.html",
+            templateUrl: "./views/produto-detalhe.html",
             controller: "ProdutoDetalheController"
 
         })
         .when("/carrinho", {
-            templateUrl: "../views/carrinho.html",
+            templateUrl: "./views/carrinho.html",
             controller: "CarrinhoController"
 
         })
         .when("/checkout", {
-            templateUrl: "../views/checkout.html",
+            templateUrl: "./views/checkout.html",
             controller: "CheckoutController"
 
         })
         .when("/checkout-result", {
-            templateUrl: "../views/checkout-result.html",
+            templateUrl: "./views/checkout-result.html",
             controller: "CheckoutResultController"
 
         })
         .otherwise({
             redirectTo: '/'
         });
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
-    $locationProvider.hashPrefix('!');
+    // $locationProvider.html5Mode({
+    //     enabled: true,
+    //     requireBase: false
+    // });
+    // $locationProvider.hashPrefix('!');
 }]);
